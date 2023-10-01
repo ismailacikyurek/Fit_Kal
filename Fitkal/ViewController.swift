65500f993a13a4c459b29c8c602275bc05302cc5
ismailacikyurek <ismailackyurek@gmail.com>
2023-10-01 19:13:57 +0300

    fitkal did begin

---
+++ Fitkal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fitkal
+//
+//  Created by İSMAİL AÇIKYÜREK on 1.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+}
+
